740ca002134a1911455e1cdbe60826fdb35da68a
Slava <fender707@rambler.ru>
2015-12-30 14:32:21 +0200

    Production version of website

--- php/mail.php
+++ php/mail.php
@@ -1,6 +1,4 @@
 <?php
-require("PHPMailerAutoload.php");
-
 
 	//form validation vars
 	$formok = true;
@@ -15,38 +13,13 @@ require("PHPMailerAutoload.php");
 	$name = $_POST['name'];	
 	$email = $_POST['email'];
 	$subject = $_POST['subject'];
-	$message = $_POST['message'];
+	$comments = $_POST['message'];
 
+	$message = "Topic: " . $subject . " \r\n";
+	$message .= "Email Address: " . $email . " \r\n";
+	$message .= "Comments: " . $comments . " \r\n";
 
-$mail = new PHPMailer();
-
-$mail->IsSMTP(); // telling the class to use SMTP
-$mail->SMTPDebug  = 0;                     // enables SMTP debug information (for testing)
-$mail->SMTPAuth   = true;                  // enable SMTP authentication
-$mail->SMTPSecure = "tls";                 // sets the prefix to the servier
-$mail->Host       = "smtp.gmail.com";      // sets GMAIL as the SMTP server
-$mail->Port       = 587;                   // set the SMTP port for the GMAIL server
-$mail->Username   = "test@gmail.com";      // GMAIL username
-$mail->Password   = "*******";             // GMAIL password
-
-$mail->SetFrom($email);
-$mail->Subject    = $subject;
-$mail->MsgHTML("<p>You have recieved a new message from the enquiries form on your website.</p>
-					  <p><strong>Name: </strong> {$name} </p>
-					  <p><strong>Email Address: </strong> {$email} </p>
-					  <p><strong>Subject: </strong> {$subject} </p>
-					  <p><strong>Message: </strong> {$message} </p>
-					  <p>This message was sent from the IP Address: {$ipaddress} on {$date} at {$time}</p>");
-$mail->AddAddress("fender707@rambler.ru");
-
-if(!$mail->Send())
-{
-   echo "Mail Not Sent <p>";
-   echo "Mailer Error: " . $mail->ErrorInfo;
-   exit;
-}
-
-echo "Mail Sent";
-
+	mail("hello@secventures.com", "Contact Us Form", $message, "From: support@secventures.com");
 
 ?>
+
